c599a50238592e17bf8138a4f9feeae1386fd431
Nick Perri <nperri6@gmail.com>
2015-12-14 15:25:18 -0500

    Add factory for user

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    username {Faker::Internet.user_name}
+    password {Faker::Internet.password}
+    firstname {Faker::Name.first_name}
+    lastname {Faker::Name.last_name}
+    birthday {"01/01/1990"}
+    email {Faker::Internet.email}
   end
-
 end
