ee6b80fbb45abba64508bb5068a7278b97a21eb2
Nick Perri <nperri6@gmail.com>
2015-12-14 12:58:39 -0500

    Create route to signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       session[:user_id] = @user.id
+      flash[:notice] = "Welcome to Health Planner"
       redirect_to root_path
     else
       render :new
