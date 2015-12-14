0ace18c0c2142a4d589188824c76f1faae3fe49a
Nick Perri <nperri6@gmail.com>
2015-12-14 16:01:17 -0500

    Add coverage file

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+[![Circle CI](https://circleci.com/gh/nperri6/health_planner.svg?style=shield&circle-token=:circle-token)](https://circleci.com/gh/nperri6/health_planner)
+
 This website will help you plan out your doctors appointments.  Between your general care doctor, specialists, dentists, orthodontists, etc. it  can be hard to keep track of
 when you are ready for an appointment.  Health Planner will help you keep track of all of your appointments in one location.
 
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  context 'new' do
+
+    it 'assigns @user to a new user' do
+      get :new
+      expect(assigns(:user)).to be_a_new(User)
+    end
+
+    it 'renders the new session template' do
+      get :new
+      expect(response).to render_template(:new)
+    end
+  end
+
+  context 'create' do
+    let(:logging_in_user) {User.create!(username: "jim", firstname: "jim", lastname: 'smith', password: 'jim', email: "jim@gmail.com", birthday: '10/02/1990')}
 
 end
