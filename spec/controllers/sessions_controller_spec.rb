5e3811eea89085235ca40350da56a14c5f9af4cd
Nick Perri <nperri6@gmail.com>
2015-12-14 16:37:49 -0500

    Add tests for destroying session and successful login

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -18,13 +18,29 @@ RSpec.describe SessionsController, type: :controller do
     let(:logging_in_user) {User.create!(username: "jim", firstname: "jim", lastname: 'smith', password: 'jim', email: "jim@gmail.com", birthday: '10/02/1990')}
 
     it 'redirects to root if valid input is entered' do
-      post :create, username: "jim", password: 'jim'
-      # expect(response).to redirect_to root_path
+      post :create, username: logging_in_user.username, password: logging_in_user.password
+      expect(response).to redirect_to root_path
     end
 
     it 'renders new if invalid input is entered' do
       post :create, username: 'dsfl', password: 'wfws'
       expect(response).to render_template(:new)
     end
+  end
+
+  context 'destroy' do
+    let(:logging_in_user) {User.create!(username: "jim", firstname: "jim", lastname: 'smith', password: 'jim', email: "jim@gmail.com", birthday: '10/02/1990')}
+    it 'clears the session' do
+      post :create, username: logging_in_user.username, password: logging_in_user.password
+      get :destroy
+      expect(session[:user_id]).to eq(nil)
+    end
+
+    it 'redirects to root path' do
+      post :create, username: logging_in_user.username, password: logging_in_user.password
+      get :destroy
+      expect(response).to redirect_to root_path
+    end
+
   end
 end
